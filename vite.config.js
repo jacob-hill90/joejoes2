2e7c245ab40672223a6c598257615f4530256b02
jacob-hill90 <hilljake93@gmail.com>
2024-11-17 07:06:07 -0500

    config stuff

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/joejoes2/', // Set to your repository name
+  base: '/', // For custom domain, base path is root
 });
